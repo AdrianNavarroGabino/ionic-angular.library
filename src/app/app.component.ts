654fb6f525d662b78894ec09c03c81c9b7c30ea9
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2022-06-19 17:45:18 +0200

    Press again to exit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { Platform, ToastController } from '@ionic/angular';
+const { App } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public sePuedeCerrar: boolean = false;
+
+  constructor(private platform: Platform, private toastController: ToastController) {
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      if(this.sePuedeCerrar) {
+        App.exitApp();
+      }
+      else {
+        this.sePuedeCerrar = true;
+
+        this.handleButtonClick();
+
+        setTimeout(() => this.sePuedeCerrar = false, 2500);
+      }
+    });
+  }
+
+  async handleButtonClick() {
+    const toast = await this.toastController.create({
+      duration: 2000,
+      message: 'Press again to exit'
+    });
+    await toast.present();
+  }
 }
